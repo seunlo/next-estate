4c962b4e68e72e13a17b101876518c95a8435abb
seun jacobs <seunlo@gmail.com>
2025-01-08 23:23:09 +0000

    authentication completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 
@@ -33,11 +34,16 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link href="/sign-in">
-            <li className="hidden md:inline text-slate-700 hover:underline">
-              Sign In
-            </li>
-          </Link>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <Link href="/sign-in">
+              <li className="hidden md:inline text-slate-700 hover:underline">
+                Sign In
+              </li>
+            </Link>
+          </SignedOut>
         </ul>
       </div>
     </header>
